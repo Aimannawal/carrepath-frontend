f6ebd0f4d55c600684ef0f0aacd3abc03ed13697
Rizalramzi <ri126661@gmail.com>
2026-04-10 17:46:50 +0700

    add sidebar

---
+++ app/pages/sidebar.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+    <section class="w-full h-screen bg-[#F8F9FA] text-[#292929]">
+        <aside class="w-55 px-5 py-12.5 flex flex-col gap-7.5 bg-white h-screen fixed">
+            <div class="ps-5 flex flex-row items-center justify-between">
+                <span class="text-[24px] font-semibold">Carre<span class="text-[#2B4DB6]">path.</span></span>
+            </div>
+
+            <div class="flex flex-col gap-7.5">
+                <hr>
+                <div class="flex flex-col gap-2.5">
+                    
+                </div>
+            </div>
+        </aside>
+    </section>
+</template>
